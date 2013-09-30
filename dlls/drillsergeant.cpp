955ae2e907fd3ab9c57ba816c7a429d501d9a593
Lucas <godkiller_nt@hammermaps.de>
2013-09-30 21:50:06 +0200

    Add Monster: Drill & Recruit

---
+++ dlls/drillsergeant.cpp
@@ -0,0 +1,142 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+//=========================================================
+// Drillsergeant * Training Course
+//=========================================================
+#include	"extdll.h"
+#include	"util.h"
+#include	"cbase.h"
+#include	"monsters.h"
+#include	"schedule.h"
+
+//=========================================================
+// Monster's Anim Events Go Here
+//=========================================================
+class CDrill : public CBaseMonster
+{
+public:
+	void Spawn( void );
+	void Precache( void );
+	void SetYawSpeed( void );
+	int  Classify ( void );
+	void HandleAnimEvent( MonsterEvent_t *pEvent );
+	void PainSound( void );
+
+	static const char *pPainSounds[];
+
+	int TakeDamage( entvars_t *pevInflictor, entvars_t *pevAttacker, float flDamage, int bitsDamageType );
+};
+
+LINK_ENTITY_TO_CLASS( monster_drillsergeant, CDrill );
+
+const char *CDrill::pPainSounds[] = 
+{
+	"drill/sound4.wav",
+	"drill/dr2_add18.wav",
+	"drill/dr2_add12.wav",
+	"drill/dr2_add09.wav",
+	"drill/dr2_add10.wav",
+	"drill/dr2_add03.wav",
+	"drill/dr2_add02.wav",
+};
+
+//=========================================================
+// Classify - indicates this monster's place in the 
+// relationship table.
+//=========================================================
+int	CDrill :: Classify ( void )
+{
+	return	CLASS_PLAYER_ALLY;
+}
+
+//=========================================================
+// SetYawSpeed - allows each sequence to have a different
+// turn rate associated with it.
+//=========================================================
+void CDrill :: SetYawSpeed ( void )
+{
+	pev->yaw_speed = 120;
+}
+
+int CDrill :: TakeDamage( entvars_t *pevInflictor, entvars_t *pevAttacker, float flDamage, int bitsDamageType )
+{
+	// Take 30% damage from bullets
+	if ( bitsDamageType == DMG_BULLET )
+	{
+		Vector vecDir = pev->origin - (pevInflictor->absmin + pevInflictor->absmax) * 0.5;
+		vecDir = vecDir.Normalize();
+		float flForce = DamageForce( flDamage );
+		pev->velocity = pev->velocity + vecDir * flForce;
+		flDamage *= 0.3;
+	}
+
+	// HACK HACK -- until we fix this.
+	if ( IsAlive() )
+		PainSound();
+
+	return CBaseMonster::TakeDamage( pevInflictor, pevAttacker, flDamage, bitsDamageType );
+}
+
+void CDrill :: PainSound( void )
+{
+	int pitch = 95 + RANDOM_LONG(0,9);
+
+	if (RANDOM_LONG(0,5) < 2)
+		EMIT_SOUND_DYN ( ENT(pev), CHAN_VOICE, pPainSounds[ RANDOM_LONG(0,ARRAYSIZE(pPainSounds)-1) ], 1.0, ATTN_NORM, 0, pitch );
+}
+
+//=========================================================
+// HandleAnimEvent - catches the monster-specific messages
+// that occur when tagged animation frames are played.
+//=========================================================
+void CDrill :: HandleAnimEvent( MonsterEvent_t *pEvent )
+{
+	CBaseMonster::HandleAnimEvent( pEvent );
+}
+
+//=========================================================
+// Spawn
+//=========================================================
+void CDrill :: Spawn()
+{
+	Precache( );
+
+	SET_MODEL(ENT(pev), "models/drill.mdl");
+	UTIL_SetSize( pev, VEC_HUMAN_HULL_MIN, VEC_HUMAN_HULL_MAX );
+
+	pev->solid			= SOLID_SLIDEBOX;
+	pev->movetype		= MOVETYPE_STEP;
+	m_bloodColor		= BLOOD_COLOR_RED;
+	pev->health			= 20;
+	pev->view_ofs		= VEC_VIEW;// position of the eyes relative to monster's origin.
+	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
+	m_MonsterState		= MONSTERSTATE_NONE;
+	m_afCapability		= bits_CAP_DOORS_GROUP;
+
+	MonsterInit();
+}
+
+//=========================================================
+// Precache - precaches all resources this monster needs
+//=========================================================
+void CDrill :: Precache()
+{
+	int i;
+
+	PRECACHE_MODEL("models/drill.mdl");
+
+	for ( i = 0; i < ARRAYSIZE( pPainSounds ); i++ )
+	PRECACHE_SOUND((char *)pPainSounds[i]);
+}
---
+++ dlls/recruit.cpp
@@ -0,0 +1,101 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+//=========================================================
+// Recruit * Training Course
+//=========================================================
+#include	"extdll.h"
+#include	"util.h"
+#include	"cbase.h"
+#include	"monsters.h"
+#include	"schedule.h"
+
+//=========================================================
+// Monster's Anim Events Go Here
+//=========================================================
+class CRecruit : public CBaseMonster
+{
+public:
+	void Spawn( void );
+	void SetYawSpeed( void );
+	int  Classify ( void );
+	void HandleAnimEvent( MonsterEvent_t *pEvent );
+
+	int TakeDamage( entvars_t *pevInflictor, entvars_t *pevAttacker, float flDamage, int bitsDamageType );
+};
+
+LINK_ENTITY_TO_CLASS( monster_recruit, CRecruit );
+
+//=========================================================
+// Classify - indicates this monster's place in the 
+// relationship table.
+//=========================================================
+int	CRecruit :: Classify ( void )
+{
+	return	CLASS_PLAYER_ALLY;
+}
+
+//=========================================================
+// SetYawSpeed - allows each sequence to have a different
+// turn rate associated with it.
+//=========================================================
+void CRecruit :: SetYawSpeed ( void )
+{
+	pev->yaw_speed = 120;
+}
+
+int CRecruit :: TakeDamage( entvars_t *pevInflictor, entvars_t *pevAttacker, float flDamage, int bitsDamageType )
+{
+	// Take 30% damage from bullets
+	if ( bitsDamageType == DMG_BULLET )
+	{
+		Vector vecDir = pev->origin - (pevInflictor->absmin + pevInflictor->absmax) * 0.5;
+		vecDir = vecDir.Normalize();
+		float flForce = DamageForce( flDamage );
+		pev->velocity = pev->velocity + vecDir * flForce;
+		flDamage *= 0.3;
+	}
+
+	return CBaseMonster::TakeDamage( pevInflictor, pevAttacker, flDamage, bitsDamageType );
+}
+
+//=========================================================
+// HandleAnimEvent - catches the monster-specific messages
+// that occur when tagged animation frames are played.
+//=========================================================
+void CRecruit :: HandleAnimEvent( MonsterEvent_t *pEvent )
+{
+	CBaseMonster::HandleAnimEvent( pEvent );
+}
+
+//=========================================================
+// Spawn
+//=========================================================
+void CRecruit :: Spawn()
+{
+	PRECACHE_MODEL("models/recruit.mdl");
+	SET_MODEL(ENT(pev), "models/recruit.mdl");
+	UTIL_SetSize( pev, VEC_HUMAN_HULL_MIN, VEC_HUMAN_HULL_MAX );
+
+	pev->solid			= SOLID_SLIDEBOX;
+	pev->movetype		= MOVETYPE_STEP;
+	m_bloodColor		= BLOOD_COLOR_RED;
+	pev->health			= 20;
+	pev->view_ofs		= VEC_VIEW;// position of the eyes relative to monster's origin.
+	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
+	m_MonsterState		= MONSTERSTATE_NONE;
+	m_afCapability		= bits_CAP_DOORS_GROUP;
+
+	MonsterInit();
+}
